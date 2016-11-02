bab64b7167a477081f67e4237143f05a8bc9c8f4
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-11-02 16:53:23 +0000

    Add HTTP request function to get image description from URL

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var button = document.querySelector('.button');
-var imageURL;
+var imageURL, imageDescription;
 //
 // button.onclick = function() {
 //   image = "https://source.unsplash.com/random";
@@ -12,6 +12,7 @@ generateImage.onreadystatechange = function() {
   if (generateImage.readyState === 4 && generateImage.status == 200) {
     console.log(generateImage.response);
     imageURL = JSON.parse(generateImage.response).urls.regular;
+    // getImageDescription(imageURL);
     document.querySelector('.image').src = imageURL;
   }
 }
@@ -20,3 +21,20 @@ button.onclick = function() {
   generateImage.open('GET', "https://api.unsplash.com/photos/random?client_id=" + unsplashKey, true);
   generateImage.send();
 };
+
+var describeImage = new XMLHttpRequest();
+
+describeImage.onreadystatechange = function() {
+  if (describeImage.readyState === 4 && describeImage.status == 200) {
+    imageDescription = JSON.parse(describeImage.response).description.captions.text;
+    console.log(imageDescription);
+  }
+}
+
+var getImageDescription = function (url) {
+  describeImage.open('POST', "https://api.projectoxford.ai/vision/v1.0/describe?maxCandidates=1");
+  describeImage.setRequestHeader("Content-Type", "application/json");
+  describeImage.setRequestHeader("Ocp-Apim-Subscription-Key", computerVisionKey);
+  var body = JSON.stringify({url : url});
+  describeImage.send(body);
+}
