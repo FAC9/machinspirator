c67a9a5a5d00bc0f250eea6ff9b119809c795553
msachi <marchee88@gmail.com>
2016-11-02 17:04:29 +0000

    Add initial DOM tests

--- spec/api_spec.js
+++ spec/api_spec.js
@@ -1,3 +1,24 @@
 QUnit.test( "true returns true", function(assert) {
   assert.equal(true, true);
 });
+
+QUnit.test( "should have a div with class imageContainer", function(assert) {
+  assert.equal(document.querySelector('.imageContainer').tagName, 'DIV');
+});
+
+QUnit.test( "should have 3 buttons with class button", function(assert) {
+  assert.equal(document.getElementsByClassName('button').length, 3);
+});
+
+QUnit.test( "no image is sourced before Generate image button clicked", function(assert) {
+  assert.notOk(document.querySelector('.image').getAttribute('src'));
+});
+
+QUnit.test( "clicking Generate image button generates image", function(assert) {
+  var done = assert.async();
+  document.getElementById("generateImageButton").click();
+  window.setTimeout(function() {
+    assert.ok(document.querySelector('.image').getAttribute('src'));
+    done();
+  }, 500);
+});
