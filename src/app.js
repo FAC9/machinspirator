49c0267c35babe1e5391d304b8842403f9171a78
msachi <marchee88@gmail.com>
2016-11-03 14:43:20 +0000

    Add song API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-var button = document.querySelector('.button');
-var imageURL, imageDescription;
+var generateImageButton = document.querySelector('#generate-image-button');
+var loadTuneButton = document.querySelector('#load-tune-button');
+var imageURL, imageDescription, imageTags, generateTune2, newURL;
 
 var generateImage = new XMLHttpRequest();
 
@@ -12,7 +13,7 @@ generateImage.onreadystatechange = function() {
   }
 }
 
-button.onclick = function() {
+generateImageButton.onclick = function() {
   generateImage.open('GET', "https://api.unsplash.com/photos/random?client_id=" + unsplashKey, true);
   generateImage.send();
 };
@@ -23,6 +24,7 @@ describeImage.onreadystatechange = function() {
   if (describeImage.readyState === 4 && describeImage.status == 200) {
     imageDescription = JSON.parse(describeImage.response).description.captions[0].text;
     console.log(imageDescription);
+    imageTags = JSON.parse(describeImage.response).description.tags;
     document.querySelector(".image-description").textContent = imageDescription;
   }
 }
@@ -34,3 +36,29 @@ var getImageDescription = function (url) {
   var body = JSON.stringify({url : url});
   describeImage.send(body);
 }
+
+var generateTune = new XMLHttpRequest();
+
+// generateTune2 = new XMLHttpRequest();
+// generateTune2.onreadystatechange = function () {
+//   if (generateTune2.readyState === 4 && generateTune2.status == 200) {
+//     var youtubeURL = JSON.parse(generateTune2.response).videos[0].uri;
+//     var youtubeTitle = JSON.parse(generateTune2.response).videos[0].title;
+//     document.querySelector(".youtube-link").innerHTML = '<a href="' + youtubeURL + '" target="_blank">' + youtubeTitle + '</a>';
+//   }
+// }
+
+generateTune.onreadystatechange = function() {
+  if (generateTune.readyState === 4 && generateTune.status == 200) {
+    document.querySelector(".youtube-link").innerHTML = JSON.parse(generateTune.response).results[0].title;
+    //console.log(JSON.parse(generateTune.response).results[0].resource_url);
+    // newURL = JSON.parse(generateTune.response).results[0].resource_url;
+    // generateTune2.open('GET', newURL, true);
+    // generateTune2.send();
+  }
+}
+
+loadTuneButton.onclick = function() {
+  generateTune.open('GET', "https://api.discogs.com/database/search?release_title=" + imageTags[0] + "&key=" + discogsKey + "&secret=" + discogsSecret, true);
+  generateTune.send();
+};
