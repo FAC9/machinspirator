46bd6bcbefe30c87f0629fa56d941f1e26f5644a
denesnori <denes.nori@yahoo.com>
2016-11-03 11:45:48 +0000

    add guardian api call

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-var button = document.querySelector('.button');
-var imageURL, imageDescription;
+var generateImageButton = document.querySelector('#generateImageButton');
+var guardianButton = document.querySelector('#guardianButton');
+var imageURL, imageDescription,imageTags,guardianNews;
 //
 // button.onclick = function() {
 //   image = "https://source.unsplash.com/random";
@@ -10,14 +11,14 @@ var generateImage = new XMLHttpRequest();
 
 generateImage.onreadystatechange = function() {
   if (generateImage.readyState === 4 && generateImage.status == 200) {
-    console.log(generateImage.response);
+  //  console.log(generateImage.response);
     imageURL = JSON.parse(generateImage.response).urls.regular;
      getImageDescription(imageURL);
     document.querySelector('.image').src = imageURL;
   }
 }
 
-button.onclick = function() {
+generateImageButton.onclick = function() {
   generateImage.open('GET', "https://api.unsplash.com/photos/random?client_id=" + unsplashKey, true);
   generateImage.send();
 };
@@ -27,8 +28,10 @@ var describeImage = new XMLHttpRequest();
 describeImage.onreadystatechange = function() {
   if (describeImage.readyState === 4 && describeImage.status == 200) {
     imageDescription = JSON.parse(describeImage.response).description.captions[0].text;
-    console.log(imageDescription);
+    console.log(JSON.parse(describeImage.response).description.tags);
+    imageTags = JSON.parse(describeImage.response).description.tags;
     document.querySelector(".image-description").textContent = imageDescription;
+    document.querySelector(".image-tags").textContent = imageTags;
   }
 }
 
@@ -39,3 +42,25 @@ var getImageDescription = function (url) {
   var body = JSON.stringify({url : url});
   describeImage.send(body);
 }
+
+var generateGuardian = new XMLHttpRequest();
+
+guardianButton.onclick = function(){
+  //http://content.guardianapis.com/search?q=water%20bird&api-key=test
+  generateGuardian.open('get',"http://content.guardianapis.com/search?q="+imageTags[0]+"%20"+imageTags[1]+"%20"+imageTags[2]+"&api-key="+guardianKey);
+  generateGuardian.send();
+};
+
+generateGuardian.onreadystatechange = function(){
+  if (generateGuardian.readyState === 4 && generateGuardian.status === 200){
+    guardianNews = JSON.parse(generateGuardian.response);
+    var res = "";
+    for (var i=0; i< guardianNews.response.results.length;i++)
+    {
+      res += "<p>"+guardianNews.response.results[i].webTitle+"</p>";
+      res += "<p>"+guardianNews.response.results[i].webUrl+"</p>";
+    }
+    console.log(JSON.parse(generateGuardian.response).response.results);
+    document.querySelector(".articles").innerHTML = res;
+  }
+};
