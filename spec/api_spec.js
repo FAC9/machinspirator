d3dc4831ffa298e55dbb7319d75c4bd7212afd82
msachi <marchee88@gmail.com>
2016-11-04 12:38:40 +0000

    Add/fix two basic tests

--- spec/api_spec.js
+++ spec/api_spec.js
@@ -2,19 +2,25 @@ QUnit.test( "should have a div with class image-container", function(assert) {
   assert.equal(document.querySelector('.image-container').tagName, 'DIV');
 });
 
-QUnit.test( "should have 3 buttons with class button", function(assert) {
-  assert.equal(document.getElementsByClassName('button').length, 3);
+QUnit.test( "should have GenerateImage button", function(assert) {
+  assert.equal(document.querySelector("#generate-image-button").tagName, 'BUTTON');
 });
 
-QUnit.test( "no image is sourced before Generate image button clicked", function(assert) {
+QUnit.test( "no content is sourced before Generate image button clicked", function(assert) {
   assert.notOk(document.querySelector('.image').getAttribute('src'));
+  assert.notOk(document.querySelector('.image-description').innerHTML);
+  assert.notOk(document.querySelector('.articles').innerHTML);
+  assert.notOk(document.querySelector('.youtube-link').innerHTML);
 });
 
-QUnit.test( "clicking Generate image button generates image", function(assert) {
+QUnit.test( "clicking Generate image button generates image, description, articles and song", function(assert) {
   var done = assert.async();
-  document.getElementById("generate-image-button").click();
+  document.querySelector("#generate-image-button").click();
   window.setTimeout(function() {
     assert.ok(document.querySelector('.image').getAttribute('src'));
+    assert.ok(document.querySelector('.image-description').innerHTML);
+    assert.ok(document.querySelector('.articles').innerHTML);
+    assert.ok(document.querySelector('.youtube-link').innerHTML);
     done();
-  }, 500);
+  }, 7000);
 });
